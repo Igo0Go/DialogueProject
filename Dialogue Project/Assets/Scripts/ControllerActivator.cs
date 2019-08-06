c7d8abf1876ab2a5ec822a6424e4bf7aa1c6ebb3
Igo0Go <chudakin98@gmail.com>
2019-08-06 21:41:33 +0700

    Титры

---
+++ Dialogue Project/Assets/Scripts/ControllerActivator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerActivator : MonoBehaviour
+{
+    public void ControllerOn()
+    {
+        Invoke("Action", 3);
+    }
+
+    private void Action()
+    {
+        GetComponent<ControllerProcessor>().enabled = true;
+    }
+}
